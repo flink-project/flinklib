810e37176f8e305a24032b920cdebdbd35a9ea8c
MarcelGehrig <marcel.gehrig@ntb.ch>
2016-01-12 17:03:32 +0100

    "subdevtypes.c" is now generated with CMake

--- lib/subdevtypes.c
+++ lib/subdevtypes.c
@@ -12,58 +12,24 @@
  *                                                                 *
  *******************************************************************/
 
-/** @file flinkioctl_cmd.h
+/** @file subdevtypes.c
  *  @brief flink userspace library, strings for subdevice functions.
  *
- *  This header file contains string definitions for subdevice function id's.
+ *  "subdevtypes.c" only includes "subdevtypes.c.inc" which is created
+ *   automatically.
+ * 
+ *  "subdevtypes.c.inc" is a header file which contains string
+ *   definitions for subdevice function id's.
  *
  *  THIS FILE WAS CREATED AUTOMATICALLY
  *
- *  Createt with: flinklinux/shared_config/create_ioctl_files.sh
+ *  "subdevtypes.c.inc" is created with:
+ * 	flinkinterface/func_id/create_subdevtypes.c.sh
  *
  *
- *  @author Martin Züger
  *  @author Marcel Gehrig
  */
  
 
-#include <stdint.h>
+#include <subdevtypes.c.inc>
  
- const char* flink_subdev_function_strings[] = {
-	"Info",			// 0x00",
-	"Analog input",			// 0x01",
-	"Analog output",			// 0x02",
-	"reserved",			// 0x03",
-	"reserved",			// 0x04",
-	"Digital I/O",			// 0x05",
-	"Counter",			// 0x06",
-	"Timer",			// 0x07",
-	"Memory",			// 0x08",
-	"reserved",			// 0x09",
-	"reserved",			// 0x0A",
-	"reserved",			// 0x0B",
-	"PWM",			// 0x0C",
-	"PPWA",			// 0x0D",
-	"unknown",			// 0x0E",
-	"unknown",			// 0x0F",
-	"Watch dog",			// 0x10",
-	"Sensor",			// 0x11",
-	"unknown",			// 0x12",
-	"unknown",			// 0x13",
-	"unknown",			// 0x14",
-	"unknown",			// 0x15",
-	"unknown",			// 0x16",
-	"unknown",			// 0x17",
-	"unknown",			// 0x18",
-	"unknown",			// 0x19",
-};
- #define NOF_KNOWNSUBDEVIDS (sizeof(flink_subdev_function_strings) / sizeof(char*))
- 
- 
- const char* flink_subdevice_id2str(uint16_t id) {
- if(id > NOF_KNOWNSUBDEVIDS - 1) { // unknown subdevice id
- return unknown;
- }
- return flink_subdev_function_strings[id];
- }
-
